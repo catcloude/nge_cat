64062cab6e1dcbe46522aac1659b87e9150e6ba6
catcloud <cat086424@gmail.com>
2026-06-07 21:15:17 +0200

    Create install.sh for moving config files
    Add installation script to move configuration files.

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "i hope you have everything intsalled"
+echo
+echo "now the files will be moved to thier needed place"
+mv hypr ~/.config
+mv fastfetch ~/.config
+mv alacritty ~/.config
+mv waybar ~/.confg
+echo
+echo "everythiin is done enjoy your new rice"
