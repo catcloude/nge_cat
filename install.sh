53477ccf694de213b7d6ddd7645edc803647bb44
catcloud <cat086424@gmail.com>
2026-06-09 19:40:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-echo "insatling the dependencies"
+echo "instling the dependencies"
 sudo pacman -Sy cava fastfetch waybar otf-monaspace-nerdfonts alacritty
 echo "now the files will be moved to thier needed place"
 wget https://i.pinimg.com/736x/3d/d4/c2/3dd4c2a22ba8b87a74a4c6f5d31e7272.jpg
@@ -12,4 +12,4 @@ mv fastfetch ~/.config
 mv alacritty ~/.config
 mv waybar ~/.confg
 echo
-echo "everythiin is done enjoy your new rice"
+echo "everything is done enjoy your new rice"
